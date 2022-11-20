a2ccba74b026d52658603ac244ef5078853cd959
Jose Barrios <jose.barrios@students.snow.edu>
2022-11-20 01:23:19 -0700

    commit

--- Monopoly.Test/UnitTest1.cs
+++ Monopoly.Test/UnitTest1.cs
@@ -10,8 +10,28 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestBoard()
     {
-        Assert.Pass();
+        Board board = new Board();
+        string boardstring = board.GetBoardAsString();
+        Assert.AreEqual(boardstring, "");
+
+
+    }
+    [Test]
+    public void TestFreeParkingLot()
+    {
+        FreeParkingLot freeparkinglot = new FreeParkingLot();
+        string IdOfFreeParkingLot = freeparkinglot.Id = "Jose";
+        Assert.AreEqual(IdOfFreeParkingLot, "Jose");
+    }
+    [Test]
+    public void TestPLayers()
+    {
+        Player player = new Player();
+        string playername = player.name = "Johnny";
+        string playersmoney = player.Money = 2500;
+        Assert.AreEqual(playersname, "Johnny");
+        Assert.AreEqual(playersmoney, 2500);
     }
 }
